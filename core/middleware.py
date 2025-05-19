a4f3e7bac2b5e3561c9dbb9e8b792d8f57936abb
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-05-19 20:41:03 +0530

    Add '/config' to the list of open routes in middleware

--- core/middleware.py
+++ core/middleware.py
@@ -24,7 +24,7 @@ async def token_verification_middleware(request: Request, call_next):
     logger.info(f"Request received from origin: {origin or 'Unknown'}")
 
     # Allow public routes
-    open_routes = ["/auth", "/openapi.json", "/auth/user/reg", "/auth/login", "/docs", "/triggers"]
+    open_routes = ["/auth", "/openapi.json", "/auth/user/reg", "/auth/login", "/docs", "/triggers", "/config"]
     if request.url.path == "/" or request.url.path == "/budget/email" or any(
             request.url.path.startswith(route) for route in open_routes):
         return await call_next(request)
